fc820c3957c1cbffdbd859db5b5cd031c8a6c5a6
Gwangyong <key4168@icloud.com>
2025-07-08 14:05:37 +0900

    ✨ 통화 환율 모델 Currency 정의 (#2)
    - API 응답을 기반으로 base_code, rates 필드 포함
    - Codable 및 CodingKeys로 키 매핑 처리

--- HwanYulApp/Model/Currency.swift
+++ HwanYulApp/Model/Currency.swift
@@ -5,3 +5,12 @@
 //  Created by 서광용 on 7/8/25.
 // MARK: 통화 + 환율 정보 모델
 
+struct Currency: Codable {
+  let baseCode: String
+  let rates: [String: Double]
+  
+  enum CodingKeys: String, CodingKey {
+    case baseCode = "base_code"
+    case rates
+  }
+}
