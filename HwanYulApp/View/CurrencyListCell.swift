7d1e8c9a48e7f354dfe56ce79265d3a1d2751f4c
Gwangyong <key4168@icloud.com>
2025-07-08 17:51:53 +0900

    🎨 환율 리스트 셀 UI 구성 완료 (#2)
    - 통화 코드 및 환율 라벨 추가
    - SnapKit으로 셀 내 레이아웃 설정
    - 더미 텍스트로 초기 UI 확인용 설정

--- HwanYulApp/View/CurrencyListCell.swift
+++ HwanYulApp/View/CurrencyListCell.swift
@@ -12,11 +12,43 @@ final class CurrencyListCell: UITableViewCell {
   
   static let identifier = "CurrencyListCell"
   
+  private let currencyCodeLabel: UILabel = { // 통화 코드
+    let label = UILabel()
+    label.text = "KRW"
+    label.textColor = .label
+    return label
+  }()
+  
+  private let exchangeRateLabel: UILabel = { // 환율
+    let label = UILabel()
+    label.text = "10123"
+    label.textColor = .label
+    return label
+  }()
+  
   override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
     super .init(style: style, reuseIdentifier: reuseIdentifier)
+    configureUI()
   }
   
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
+  }
+  
+  // MARK: - configureUI
+  private func configureUI() {
+    [currencyCodeLabel, exchangeRateLabel].forEach {
+      contentView.addSubview($0)
+    }
+    
+    currencyCodeLabel.snp.makeConstraints {
+      $0.centerY.equalToSuperview()
+      $0.leading.equalToSuperview().inset(16)
+    }
+    
+    exchangeRateLabel.snp.makeConstraints {
+      $0.centerY.equalToSuperview()
+      $0.trailing.equalToSuperview().inset(16)
+    }
   }
 }
